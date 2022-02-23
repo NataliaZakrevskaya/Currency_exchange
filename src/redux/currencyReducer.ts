1ff285e9ef41b66c2eddaee999f91080310e49c2
Natalia <zakrevskaya.97@inbox.ru>
2022-02-23 16:44:43 +0300

    currencyReducer is done

--- src/redux/currencyReducer.ts
+++ src/redux/currencyReducer.ts
@@ -1,4 +1,4 @@
-import {CurrencyReducersTypes} from "./actions";
+import {ACTIONS_TYPE, CurrencyReducersTypes} from "./actions";
 
 export type CurrencyType = {
     currencyName: string;
@@ -38,10 +38,27 @@ export const initialState: CurrencyState = {
 }
 
 
-
 export const currencyReducer = (state: CurrencyState = initialState, action: CurrencyReducersTypes): CurrencyState => {
     switch (action.type) {
-
+        case ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE:
+            return {
+                ...state,
+                ...action.payload,
+            }
+        case ACTIONS_TYPE.CHANGE_CHANGE_ACTION:
+            return {
+                ...state,
+                ...action.payload,
+                amountOfBYN: '',
+                amountOfCurrency: '',
+            }
+        case ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY:
+            return {
+                ...state,
+                ...action.payload,
+                amountOfBYN: '',
+                amountOfCurrency: '',
+            }
         default:
             return state;
     }
